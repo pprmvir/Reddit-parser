8f7eb68ec362413d25d38efe7a230225636b990e
Paramvir Singh <pprmvir@gmail.com>
2015-07-19 10:11:29 +0530

    Update subRedditsList.py

--- subRedditsList.py
+++ subRedditsList.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup as bs
 from csv import writer
-from time import sleep
 import requests as req
-import cookielib
-import mechanize
+
 
 def write_to_csv(l):
         n = open('SUBS.csv','w')
@@ -20,69 +18,68 @@ cookie = { # this cookie lets you bypass the age filter
             'path' : '/',
             'domain' : '.reddit.com'
             }
+     
+link_to_sub_reddits = 'http://www.reddit.com/subreddits?count=7100&after=t5_2sz27'
 
-try:        
-        link_to_sub_reddits = 'http://www.reddit.com/subreddits'
+L=[]
+navigate_the_pages=1
 
-        L=[]
-        navigate_the_pages=1
-
-        while navigate_the_pages:
+while navigate_the_pages:
                 
-                if link_to_sub_reddits == 'end':
-                        break
-                
-                try:
+        if link_to_sub_reddits == 'end':
+                break
                         
-                        res = req.get(link_to_sub_reddits,headers=headers,timeout=10
-                                      , cookies=cookie)
-                        print navigate_the_pages
-                        navigate_the_pages=navigate_the_pages+1                        
+        try:                
+                                
+                res = req.get(link_to_sub_reddits,headers=headers,timeout=10
+                                                      , cookies=cookie)
+                print navigate_the_pages
+                navigate_the_pages=navigate_the_pages+1                        
+                                        
+        except req.ConnectionError as e:
+                print e                        
+                print len(L)
+                print link_to_sub_reddits
+                print 'writing to .csv .......'
+                write_to_csv(L)
+                print 'done'
+                break
+                                
+        soup = bs(res.text,"html.parser")
+                        # soup created
+
+        div = soup.body.find('div', class_=lambda(class_):class_ and class_=='content')
+        div = div.find('div', id= lambda(id):id and id=='siteTable')
+
+        cnt=0
+
+        for iterator in div:
+
+                div_thing = div.contents[cnt]
+
+                            
+                if div_thing:                        
+                        div_midcol = div_thing.find('img')                            
+                                                                      
+                        if div_midcol and div_midcol['title']=='over18':
+                                hijj = 'llsls' # just command to fill in the
+                                # space of if clause, feel free to change
+                                # of you wish
+                        else:                                    
+                                div_midcol = None 
+                else:
+                        div_midcol = None 
                         
-                except req.ConnectionError as e:
-                        print e                        
-                        print len(L)
-                        print link_to_sub_reddits
-                        print 'writing to .csv .......'
-                        write_to_csv(L)
-                        print 'done'
-                        break
-                
-                soup = bs(res.text,"html.parser")
-                # soup created
-
-                div = soup.body.find('div', class_=lambda(class_):class_ and class_=='content')
-                div = div.find('div', id= lambda(id):id and id=='siteTable')
-
-                cnt=0
-
-                for iterator in div:
-
-                    div_thing = div.contents[cnt]
-
-                    
-                    if div_thing:
-                            div_midcol = div_thing.find('img')                            
-                                                      
-                            if div_midcol and div_midcol['title']=='over18':
-                                    hijj = 'llsls' # just command to fill in the
-                                    # space of if clause, feel free to change
-                                    # of you wish
-                            else:                                    
-                                    div_midcol = None 
-                    else:
-                            div_midcol = None 
-        
-                    if div_midcol and not div_thing=='' and div_thing.name=='div' and 'thing' in div_thing['class']:
-                    # if you want sfw subs, replace "div_midcol"  with "not div_midcol"
-                    # in the above if clause
-                        
+                if not div_midcol and not div_thing=='' and div_thing.name=='div' and 'thing' in div_thing['class']:
+                        # if you want nsfw subs, replace "not div_midcol"  with "div_midcol"
+                        # in the above if clause
+                                        
                         div_entry = div_thing.find('div',class_=lambda(class_):class_ and 'entry' in class_)
-                        # div with class='entry......'
-                        
+                                                # div with class='entry......'
+                                                
                         link = div_entry.find('a')['href']
                         print link
-                        # link of the subreddit
+                                                # link of the subreddit
                         name_of_sub = link.split('/')[-2]
                         # http://www.reddit.com/subreddits/
                         # ['http:', '', 'www.reddit.com', 'subreddits', '']
@@ -105,20 +102,17 @@ try:
                         temp = [s.encode('ascii','ignore') for s in temp]
                         L.append(temp)
 
-                    elif not div_thing=='' and div_thing.name=='div' and 'nav-buttons' in div_thing['class']:
-                        # case when we find 'nav' button
+                elif not div_thing=='' and div_thing.name=='div' and 'nav-buttons' in div_thing['class']:
+                # case when we find 'nav' button
 
                         link_to_sub_reddits = div_thing.find('a',rel = 'nofollow next')
-                        
+                                                
                         if link_to_sub_reddits:
                                 link_to_sub_reddits = link_to_sub_reddits['href']
                         else:
                                 link_to_sub_reddits = 'end'
-                        break
+                                break
 
-                    cnt = cnt + 1
-                print 'NEXT LINK TO VISIT'
-                print link_to_sub_reddits                
-            
-except req.ConnectionError as e:        
-        print e
+                cnt = cnt + 1
+        print 'NEXT LINK TO VISIT'
+        print link_to_sub_reddits
